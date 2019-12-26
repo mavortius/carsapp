ed5586741034b5f3722b7ff7e1b5cd7e568e61a0
Marcelo Martins <m.a.martins@live.com>
2019-12-26 11:18:28 -0300

    Added styled input controls to dialogs

--- src/components/AddCar.tsx
+++ src/components/AddCar.tsx
@@ -4,6 +4,7 @@ import DialogTitle from "@material-ui/core/DialogTitle";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogActions from "@material-ui/core/DialogActions";
 import { Car } from "../Car";
+import { Button, TextField } from "@material-ui/core";
 
 interface IProps {
   addCar: (car: Car) => void;
@@ -59,10 +60,7 @@ class AddCar extends React.Component<IProps, IState> {
     this.setState({ open: false });
   };
 
-  handleChange = (event:
-                    | React.ChangeEvent<HTMLInputElement>
-                    | React.ChangeEvent<HTMLTextAreaElement>
-                    | React.ChangeEvent<HTMLSelectElement>) => {
+  handleChange = (event: any) => {
     this.setState({ car: { ...this.state.car, [event.target.name]: event.target.value } });
   };
 
@@ -77,20 +75,20 @@ class AddCar extends React.Component<IProps, IState> {
         <Dialog open={this.state.open} onClose={this.handleClose}>
           <DialogTitle>New car</DialogTitle>
           <DialogContent>
-            <input type="text" placeholder="Brand" name="brand" value={this.state.car.brand}
-                   onChange={this.handleChange}/><br/>
-            <input type="text" placeholder="Model" name="model" value={this.state.car.model}
-                   onChange={this.handleChange}/><br/>
-            <input type="text" placeholder="Color" name="color" value={this.state.car.color}
-                   onChange={this.handleChange}/><br/>
-            <input type="text" placeholder="Year" name="year" value={this.state.car.year}
-                   onChange={this.handleChange}/><br/>
-            <input type="text" placeholder="Price" name="price" value={this.state.car.price}
-                   onChange={this.handleChange}/><br/>
+            <TextField autoFocus fullWidth label="Brand" name="brand" value={this.state.car.brand}
+                   onChange={this.handleChange}/>
+            <TextField fullWidth label="Model" name="model" value={this.state.car.model}
+                   onChange={this.handleChange}/>
+            <TextField fullWidth label="Color" name="color" value={this.state.car.color}
+                   onChange={this.handleChange}/>
+            <TextField fullWidth label="Year" name="year" value={this.state.car.year}
+                   onChange={this.handleChange}/>
+            <TextField fullWidth label="Price" name="price" value={this.state.car.price}
+                   onChange={this.handleChange}/>
           </DialogContent>
           <DialogActions>
-            <button onClick={this.handleClose}>Cancel</button>
-            <button onClick={this.handleSave}>Save</button>
+            <Button color="secondary" onClick={this.handleClose}>Cancel</Button>
+            <Button color="primary" onClick={this.handleSave}>Save</Button>
           </DialogActions>
         </Dialog>
       </div>
